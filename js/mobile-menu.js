1615f6a8b58bf191fd56f3f5e3bc59ed0353f0f8
JoeShvartsev <120992324+JoeShvartsev@users.noreply.github.com>
2023-02-14 19:56:53 +0200

    body/footer

---
+++ js/mobile-menu.js
@@ -0,0 +1,15 @@
+
+(() => {
+    const refs = {
+        openMenulBtn: document.querySelector("[data-menu-open]"),
+        closeMenuBtn: document.querySelector("[data-menu-close]"),
+        menu: document.querySelector("[data-menu]"),
+    };
+    
+    refs.openMenulBtn.addEventListener("click", toggleMenu);
+    refs.closeMenuBtn.addEventListener("click", toggleMenu);
+    
+    function toggleMenu() {
+        refs.menu.classList.toggle("is-hidden");
+    }
+    })();
